333b5fc524a2240e29f6430bd6c27e9f3c827c59
Rubén García <rubegartor@gmail.com>
2020-04-29 01:09:09 +0200

    fix: Eliminar tema proton de los asset y sacarlo desde los node_modules

--- run-script.sh
+++ run-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp -r node_modules/@fortawesome/fontawesome-free/webfonts app/assets/css
 cp -r app/assets/sass/Commons/Proton/images app/assets/css
-cp -r app/assets/sass/Commons/Proton/fonts app/assets/css
+cp -r node_modules/jstree-bootstrap-theme/dist/themes/proton app/assets/css
 node-sass app/assets/sass/app.scss app/assets/css/app.css
 electron main.js
