18145ad38c055dc17f008d191f69ad613aa7ebe7
Rubén García <rubegartor@gmail.com>
2020-04-29 01:03:32 +0200

    feat: Mover tema proton para jstree

--- run-script.sh
+++ run-script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp -r node_modules/@fortawesome/fontawesome-free/webfonts app/assets/css
-cp -r app/assets/sass/themes/proton app/assets/css/themes/proton
+cp -r app/assets/sass/Commons/Proton/images app/assets/css
+cp -r app/assets/sass/Commons/Proton/fonts app/assets/css
 node-sass app/assets/sass/app.scss app/assets/css/app.css
 electron main.js
